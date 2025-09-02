c5c38c368849796d919c70a47d51a8149753cb28
nukopy <nukopy@gmail.com>
2025-09-03 01:49:25 +0900

    refactor: for multi language

--- app/routes.ts
+++ app/routes.ts
@@ -5,23 +5,59 @@ import {
   route,
 } from "@react-router/dev/routes";
 
-const routesJa = [
-  index("./routes/home.tsx", { id: "home-ja" }),
-  route("contents", "./routes/contents.tsx", { id: "contents-ja" }),
-  route("contents/:slug", "./routes/contents.$slug.tsx", {
-    id: "contents-slug-ja",
-  }),
-  route("tags", "./routes/tags.tsx", { id: "tags-ja" }),
-  route("tags/:tag", "./routes/tags.$tag.tsx", { id: "tags-tag-ja" }),
-];
-const routesEn = [
-  index("./routes/home.tsx", { id: "home-en" }),
-  route("contents", "./routes/contents.tsx", { id: "contents-en" }),
-  route("contents/:slug", "./routes/contents.$slug.tsx", {
-    id: "contents-slug-en",
-  }),
-  route("tags", "./routes/tags.tsx", { id: "tags-en" }),
-  route("tags/:tag", "./routes/tags.$tag.tsx", { id: "tags-tag-en" }),
-];
+const LANGUAGE_ID_MAP = {
+  ja: "ja",
+  en: "en",
+} as const;
 
-export default [...routesJa, ...prefix("en", routesEn)] satisfies RouteConfig;
+function createBaseRoutes(language: keyof typeof LANGUAGE_ID_MAP) {
+  return [
+    // root
+    index(`./routes/home.tsx`, { id: `home-${language}` }),
+
+    // /contents
+    route("contents", `./routes/contents.tsx`, {
+      id: `contents-${language}`,
+    }),
+    // /contents/:slug
+    route("contents/:slug", `./routes/contents.$slug.tsx`, {
+      id: `contents-slug-${language}`,
+    }),
+
+    // /tags
+    route("tags", `./routes/tags.tsx`, { id: `tags-${language}` }),
+    // /tags/:tag
+    route("tags/:tag", `./routes/tags.$tag.tsx`, {
+      id: `tags-slug-${language}`,
+    }),
+
+    // /misc
+    ...prefix("misc", [
+      route("test-props", `./routes/misc/test-props.tsx`, {
+        id: `misc-test-props-${language}`,
+      }),
+    ]),
+
+    // /not-found
+    route("*", `./routes/not-found.tsx`, { id: `not-found-${language}` }),
+  ];
+}
+
+function createRoutes(language: keyof typeof LANGUAGE_ID_MAP) {
+  const baseRoutes = createBaseRoutes(language);
+
+  // if ja, we don't use prefix
+  if (language === LANGUAGE_ID_MAP.ja) {
+    return baseRoutes;
+  }
+
+  // if not ja, we use prefix
+  return prefix(language, baseRoutes);
+}
+
+// create routes
+const routesJa = createRoutes(LANGUAGE_ID_MAP.ja);
+const routesEn = createRoutes(LANGUAGE_ID_MAP.en);
+const routes = [...routesJa, ...routesEn];
+
+export default routes satisfies RouteConfig;
